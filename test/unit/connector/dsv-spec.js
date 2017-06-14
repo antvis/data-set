3244f50a400ad7635a995a2d2711f21be2cdd5c4
leungwensen <leungwensen@gmail.com>
2017-06-14 18:50:57 +0800

    add test data: diamond.json

--- test/unit/connector/dsv-spec.js
+++ test/unit/connector/dsv-spec.js
@@ -18,28 +18,34 @@ function readFileSync(pathname) {
   return fs.readFileSync(path.resolve(process.cwd(), pathname), 'utf8');
 }
 
+const source = {
+  psv: readFileSync('./test/fixtures/sample.psv'),
+  csv: readFileSync('./test/fixtures/sample.csv'),
+  csv2: readFileSync('./test/fixtures/sample2.csv'),
+  tsv: readFileSync('./test/fixtures/sample.tsv'),
+  tsv2: readFileSync('./test/fixtures/sample2.tsv')
+};
+const data = [
+  {
+    Hello: '42',
+    World: '"fish"'
+  }
+];
+const data2 = [
+  {
+    Hello: '42',
+    World: '"fish"'
+  },
+  {
+    Hello: 'foo',
+    World: 'bar'
+  }
+];
+
 describe('connector: dsv', () => {
   const dsvConnector = DataSet.getConnector('dsv');
   const csvConnector = DataSet.getConnector('csv');
   const tsvConnector = DataSet.getConnector('tsv');
-
-  const data = [
-    {
-      Hello: '42',
-      World: '"fish"'
-    }
-  ];
-  const data2 = [
-    {
-      Hello: '42',
-      World: '"fish"'
-    },
-    {
-      Hello: 'foo',
-      World: 'bar'
-    }
-  ];
-
   it('api', () => {
     expect(dsvConnector.parse).to.be.a('function');
     expect(csvConnector.parse).to.be.a('function');
@@ -47,8 +53,7 @@ describe('connector: dsv', () => {
   });
 
   it('dsv', () => {
-    const psv = readFileSync('./test/fixtures/sample.psv');
-    const rows = dsvConnector.parse(psv, {
+    const rows = dsvConnector.parse(source.psv, {
       delimiter: '|'
     });
     expect(rows).to.be.an('array');
@@ -56,34 +61,52 @@ describe('connector: dsv', () => {
   });
 
   it('csv', () => {
-    const csv = readFileSync('./test/fixtures/sample.csv');
-    const rows = csvConnector.parse(csv);
+    const rows = csvConnector.parse(source.csv);
     expect(rows).to.be.an('array');
     expect(rows).to.deep.equal(data);
-    const csv2 = readFileSync('./test/fixtures/sample2.csv');
-    const rows2 = csvConnector.parse(csv2);
+    const rows2 = csvConnector.parse(source.csv2);
     expect(rows2).to.be.an('array');
     expect(rows2).to.deep.equal(data2);
   });
 
   it('tsv', () => {
-    const csv = readFileSync('./test/fixtures/sample.tsv');
-    const rows = tsvConnector.parse(csv);
+    const rows = tsvConnector.parse(source.tsv);
     expect(rows).to.be.an('array');
     expect(rows).to.deep.equal(data);
-    const tsv2 = readFileSync('./test/fixtures/sample2.tsv');
-    const rows2 = tsvConnector.parse(tsv2);
+    const rows2 = tsvConnector.parse(source.tsv2);
     expect(rows2).to.be.an('array');
     expect(rows2).to.deep.equal(data2);
   });
+});
 
+describe('DataView.source(): dsv', () => {
+  const dataSet = new DataSet();
   const DataView = DataSet.DataView;
-  it('DataView.source(): dsv', () => {
-    const dataSet = new DataSet();
-    const dataView = new DataView(dataSet);
-    const csv = readFileSync('./test/fixtures/sample.csv');
-    dataView.source(csv, {
+  let dataView;
+
+  beforeEach(() => {
+    dataView = new DataView(dataSet);
+  });
+
+  it('dsv', () => {
+    dataView.source(source.psv, {
+      type: 'dsv',
+      delimiter: '|'
+    });
+    console.log(dataView.origin);
+    expect(dataView.origin).to.deep.equal(data);
+  });
+
+  it('csv', () => {
+    dataView.source(source.csv, {
       type: 'csv'
+    });
+    expect(dataView.origin).to.deep.equal(data);
+  });
+
+  it('tsv', () => {
+    dataView.source(source.tsv, {
+      type: 'tsv'
     });
     expect(dataView.origin).to.deep.equal(data);
   });
