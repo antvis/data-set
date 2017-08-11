419a42e3ab41bec95bf8c96e67db16770368b01d
leungwensen <leungwensen@gmail.com>
2017-08-11 14:53:41 +0800

    simplify sort-by

--- src/util/partition.js
+++ src/util/partition.js
@@ -1,11 +1,11 @@
 const map = require('lodash/map');
 const groupBy = require('lodash/groupBy');
-const orderBy = require('lodash/orderBy');
+const simpleSortBy = require('./simple-sort-by');
 
 module.exports = (rows, group_by, order_by = []) => {
   let newRows = rows;
   if (order_by && order_by.length) {
-    newRows = orderBy(rows, order_by);
+    newRows = simpleSortBy(rows, order_by);
   }
   const groups = groupBy(newRows, row => {
     return map(group_by, col => row[col]).join('-');
--- test/unit/util/partition-spec.js
+++ test/unit/util/partition-spec.js
@@ -1,7 +1,13 @@
 const {
   expect
 } = require('chai');
+const {
+  cloneDeep,
+  orderBy,
+  sortBy
+} = require('lodash');
 const partition = require('../../../src/util/partition');
+const simpleSortBy = require('../../../src/util/simple-sort-by');
 
 describe('util: partition(rows, groupBy, orderBy)', () => {
   const data = [
@@ -17,5 +23,22 @@ describe('util: partition(rows, groupBy, orderBy)', () => {
     const groups = partition(data, [ 'c' ], [ 'x' ]);
     expect(groups[0].length).to.equal(2);
     expect(groups[1].length).to.equal(1);
+  });
+
+  it.skip('performance', () => {
+    // FIXME: remove `.skip` to execute performance test
+    const top2000 = require('../../fixtures/top2000.json');
+    const top2000Cloned1 = cloneDeep(top2000);
+    const top2000Cloned2 = cloneDeep(top2000);
+    const t0 = Date.now();
+    sortBy(top2000, [ 'release', 'title' ]);
+    const t1 = Date.now();
+    orderBy(top2000Cloned1, [ 'release', 'title' ]);
+    const t2 = Date.now();
+    simpleSortBy(top2000Cloned2, [ 'release', 'title' ]);
+    const t3 = Date.now();
+    expect((t3 - t2) < (t2 - t1)).to.equal(true);
+    expect((t3 - t2) < (t1 - t0)).to.equal(true);
+    // console.log(t3 - t2, t2 - t1, t1 - t0);
   });
 });
