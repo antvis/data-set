f58e16e1de0c6f3a86c9a6acda5247bf5fb3257d
leungwensen <leungwensen@gmail.com>
2017-08-22 15:58:22 +0800

    test cases and demo for regression

---
+++ test/unit/transform/regression-spec.js
@@ -0,0 +1,53 @@
+// const regression = require('regression');
+const {
+  each
+} = require('lodash');
+const {
+  expect
+} = require('chai');
+const {
+  DataSet,
+  getTransform
+} = require('../../../index');
+const {
+  REGRESSION_METHODS
+} = require('../../../src/transform/regression');
+
+describe('DataView.transform(): regression', () => {
+  const data = [];
+  for (let i = 1; i <= 10; i++) {
+    // 1~10
+    const b = i % 2;
+    data.push({
+      a: i,
+      b
+    });
+  }
+  const ds = new DataSet();
+  let dv;
+  beforeEach(() => {
+    dv = ds.createView().source(data);
+  });
+
+  it('api', () => {
+    expect(getTransform('regression')).to.be.a('function');
+  });
+
+  it('default: linear', () => {
+    dv.transform({
+      type: 'regression',
+      fields: [ 'a', 'b' ]
+    });
+    expect(dv.rows.length).to.equal(10);
+  });
+
+  each(REGRESSION_METHODS, method => {
+    it(`method: ${method}`, () => {
+      dv.transform({
+        type: 'regression',
+        fields: [ 'a', 'b' ]
+      });
+      expect(dv.rows.length).to.equal(10);
+    });
+  });
+});
