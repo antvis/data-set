6fde7b8a05343dd58a1898b0c0f5517195631e31
leungwensen <leungwensen@gmail.com>
2017-09-06 11:35:35 +0800

    enhanced partition

--- src/util/simple-sort-by.js
+++ src/util/simple-sort-by.js
@@ -1,15 +1,23 @@
+const isArray = require('lodash/isArray');
+const isFunction = require('lodash/isFunction');
+
 module.exports = (arr, keys = []) => {
-  const comparer = (a, b) => {
-    for (let i = 0; i < keys.length; i++) {
-      const key = keys[i];
-      if (a[key] < b[key]) {
-        return -1;
+  let comparer;
+  if (isFunction(keys)) {
+    comparer = keys;
+  } else if (isArray(keys)) {
+    comparer = (a, b) => {
+      for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        if (a[key] < b[key]) {
+          return -1;
+        }
+        if (a[key] > b[key]) {
+          return 1;
+        }
       }
-      if (a[key] > b[key]) {
-        return 1;
-      }
-    }
-    return 0;
-  };
+      return 0;
+    };
+  }
   return arr.sort(comparer);
 };
