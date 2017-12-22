583dbf5d41fa195764ffca491b2ef374efb17a4e
leungwensen <leungwensen@gmail.com>
2017-12-22 17:03:54 +0800

    feat(transform): hierarchy.circle-packing

---
+++ src/transform/hierarchy/pack.js
@@ -0,0 +1,59 @@
+const assign = require('lodash/assign');
+const d3Hierarchy = require('d3-hierarchy');
+const {
+  HIERARCHY,
+  registerTransform
+} = require('../../data-set');
+const {
+  getField
+} = require('../../util/option-parser');
+
+const DEFAULT_OPTIONS = {
+  field: 'value',
+  size: [ 1, 1 ], // width, height
+  padding: 0,
+  as: [ 'x', 'y' ]
+};
+
+function transform(dataView, options) {
+  if (dataView.dataType !== HIERARCHY) {
+    throw new TypeError('Invalid DataView: This transform is for Hierarchy data only!');
+  }
+  const root = dataView.root;
+  options = assign({}, DEFAULT_OPTIONS, options);
+
+  const as = options.as;
+  if (!Array.isArray(as) || as.length !== 2) {
+    throw new TypeError('Invalid as: it must be an array with 2 strings (e.g. [ "x", "y" ])!');
+  }
+
+  let field;
+  try {
+    field = getField(options);
+  } catch (e) {
+    console.warn(e);
+  }
+  if (field) {
+    root.sum(d => d[field])
+      .sort((a, b) => b[field] - a[field]);
+  }
+
+  const packLayout = d3Hierarchy.pack();
+  packLayout.size(options.size);
+
+  if (options.padding) {
+    packLayout.padding(options.padding);
+  }
+  packLayout(root);
+
+  const x = as[0];
+  const y = as[1];
+  root.each(node => {
+    node[x] = node.x;
+    node[y] = node.y;
+  });
+}
+
+registerTransform('hierarchy.pack', transform);
+registerTransform('hierarchy.circle-packing', transform);
+registerTransform('circle-packing', transform);
