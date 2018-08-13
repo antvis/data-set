705075df86cc8d945c419a16b36a60976b679ede
leungwensen <leungwensen@gmail.com>
2018-08-13 15:04:32 +0800

    chore(dev): fixing bugs cases

--- test/bugs/geo-max-call-stack-spec.js
+++ test/bugs/geo-max-call-stack-spec.js
@@ -6,8 +6,8 @@ const DataSet = require('../../src/');
 const ChinaGEO = require('../fixtures/china-geo.json');
 const Provinces = require('../fixtures/china-provinces.json');
 
-describe('DataSet', () => {
-  it('Constructor', () => {
+describe('max call stack when cloning options', () => {
+  it('geo.region', () => {
     expect(() => {
       const ds = new DataSet();
       const chinaMap = ds.createView('map').source(ChinaGEO, {
