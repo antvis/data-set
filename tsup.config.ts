c8d5fc0771f63b10b94f1299318e9bd229e93d39
yxf <1848212420@qq.com>
2024-03-18 19:30:27 +0800

    build(transform): emit index.mjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,21 +4,26 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
-  entry: Object.fromEntries(
-    globSync('src/transform/**/*.ts').map((file) => {
-      return [
-        // 这里将删除 `src/` 以及每个文件的扩展名。
-        // 因此，例如 src/nested/foo.js 会变成 nested/foo
-        path.relative('src', file.slice(0, file.length - path.extname(file).length)),
-        // 这里可以将相对路径扩展为绝对路径，例如
-        // src/nested/foo 会变成 /project/src/nested/foo.js
-        fileURLToPath(new URL(file, import.meta.url)),
-      ];
-    })
-  ),
+  entry: {
+    index: 'src/index.ts',
+    ...Object.fromEntries(
+      globSync('src/transform/**/*.ts').map((file) => {
+        return [
+          // 这里将删除 `src/` 以及每个文件的扩展名。
+          // 因此，例如 src/nested/foo.js 会变成 nested/foo
+          path.relative('src', file.slice(0, file.length - path.extname(file).length)),
+          // 这里可以将相对路径扩展为绝对路径，例如
+          // src/nested/foo 会变成 /project/src/nested/foo.js
+          fileURLToPath(new URL(file, import.meta.url)),
+        ];
+      })
+    ),
+  },
+
   format: ['esm', 'cjs'],
   outDir: 'build',
   clean: true,
+  treeshake: true,
   dts: true,
   external: ['lodash'],
   noExternal: ['dagre', 'wolfy87-eventemitter', 'd3-sankey', 'simple-statistics', 'd3-voronoi'],
